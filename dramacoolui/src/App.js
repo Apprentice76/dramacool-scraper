fc451d5776466424a9cb24086e3e08b99e1592ed
Apprentice76 <hrk.som.01@gmail.com>
2021-06-20 00:54:41 +0530

    1.2 search

--- dramacoolui/src/App.js
+++ dramacoolui/src/App.js
@@ -1,10 +1,20 @@
-import styles from './App.module.css'
+// import styles from './App.module.css'
+import { useState } from 'react';
 
 const App = () => {
-  return (
-    <div className="">
-    </div>
-  );
-}
+    const [query, setQuery] = useState('');
+
+    const updateQuery = (event) => {
+        if (event.target.value.length > 2) 		console.log(event.target.value);
+        setQuery(event.target.value);
+    };
+
+    return (
+        <div className=''>
+            <div>Search</div>
+            <input type='text' onChange={updateQuery} value={query} />
+        </div>
+    );
+};
 
 export default App;
