76b11b2370bf595a7937ce01357d22f43841203d
Apprentice76 <hrk.som.01@gmail.com>
2021-06-20 00:33:45 +0530

    1.1 reset

--- dramacoolui/src/App.js
+++ dramacoolui/src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
 import styles from './App.module.css'
 
 const App = () => {
   return (
-    <div className={styles.App}>
-      <header className={styles.Appheader}>
-        <img src={logo} className={styles.Applogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.Applink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
     </div>
   );
 }
