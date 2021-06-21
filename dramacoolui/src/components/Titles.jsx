970698047341f31eb04228a27d649f35569d249a
Apprentice76 <hrk.som.01@gmail.com>
2021-06-22 01:05:16 +0530

    midway 2.0

--- dramacoolui/src/components/Titles.jsx
+++ dramacoolui/src/components/Titles.jsx
@@ -1,10 +1,11 @@
+import styles from '../styles/Titles.module.css';
 import Title from './Title';
 
 const Titles = (props) => {
     const obj = props.data;
     const ids = Object.keys(obj);
     return (
-        <div className='container'>
+        <div className={styles.container}>
             {ids.map((id) => (
                 <Title key={id} title={obj[id]} />
             ))}
