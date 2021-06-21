55860b170029a5c63a7746df3d6d28a0d9758645
Apprentice76 <hrk.som.01@gmail.com>
2021-06-21 16:21:09 +0530

    1.4 added jssoup, axios; modified App a bit

--- dramacoolui/src/components/App.jsx
+++ dramacoolui/src/components/App.jsx
@@ -1,13 +1,29 @@
 import styles from '../styles/App.module.css'
 import { useState } from 'react';
+import JSSoup from "jssoup";
+import axios from 'axios';
+// const JSSoup = require('jssoup').default;
 
 const App = () => {
     const [query, setQuery] = useState('');
 
     const updateQuery = (event) => {
-        if (event.target.value.length > 2) 		console.log(event.target.value);
         setQuery(event.target.value);
     };
+
+    const fetchQuery = async (keyword) => {
+        const head = 'https://dramacool.lv/wp-json/dooplay/search/?keyword';
+        const tail = '&nonce=dcc0b21e4b';
+        const query = keyword.split(' ').join('%20');
+        const url = head + query + tail;
+        const res = await axios.get(url);
+        const data = res.data;
+        const soup = new JSSoup(data);
+        let x = soup.find()
+    }
+
+    // const soupify = ()
+    
 
     return (
         <div className={styles.main}>
